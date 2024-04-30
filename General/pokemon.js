b2856b6cc1342f941926a79b4bff889704dedde4
Eduard <eduard.iglesias@cirvianum.cat>
2024-04-30 18:31:48 +0200

    commit 3

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,6 +1,6 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
 function getPokemon(id) {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
       .then((res) => res.json())
       .then((pokemon) => ({
         id: pokemon.id,
@@ -18,10 +18,25 @@ function getPokemon(id) {
 
   console.log(getPokemon("bulbasaur"))
 
-  const pokemon = getPokemon(1);
-  const pokemonName = pokemon
-  console.log(pokemonName)
+  function setPokemonInfo(id, name, height, weight) {
+    try {
+      // Obtenim els elements del DOM amb `querySelector` i els emmagatzemem en una variable
+      const nameElement = document.getElementById(name);
+      const heightElement = document.querySelector(height);
+      const weightElement = document.querySelector(weight);
+  
+      // Obtenim la informació de la pel·lícula cridant al mètode de `swapi.js`
+      const pokemonInfo = getPokemon(id);
 
-  const nameElement = document.getElementById("name");
+      console.log(pokemonInfo)
+  
+      // Substituïm les dades fent servir un mètode de reemplaçament com `innerHTML`
+      nameElement.innerHTML = pokemonInfo.name;
+      heightElement.innerHTML = pokemonInfo.height;
+      weightElement.innerHTML = pokemonInfo.weight;
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 
-  nameElement.value = pokemonName;
+  setPokemonInfo(1, 'name', 'height', 'weight');
--- UF3/validacions.js
+++ UF3/validacions.js
@@ -1,73 +1,79 @@
-// A continuació et mostro algunes idees per poder començar a organitzar el teu codi.
-
-// Selecció dels inputs...
-const nameInput = document.querySelector('input[name="name"]');
-
-// Si no saps com utilitzar aquestes funcions, pots fer ús de missatges per consol o situar
-// els errors allà on consideris lògic
-
-// Funció per eliminar missatges d'error existents
-function removeExistingError(input) {
-  const existingError = input.nextElementSibling;
-  if (existingError && existingError.classList.contains("error")) {
-    existingError.remove();
-  }
-}
-
-// Funció per crear i mostrar missatges d'error
-function displayError(input, message) {
-  const error = document.createElement("div");
-  error.textContent = message;
-  error.classList.add("error");
-  //https://developer.mozilla.org/es/docs/Web/API/Element/insertAdjacentElement
-  input.insertAdjacentElement("afterend", error);
-}
-
-// Cada validació...
-
-function validateName() {
-  removeExistingError(nameInput);
-  // Validació del nom...
-
-  // Si no és vàlid...
-  displayError(nameInput, "El nom no és vàlid");
-}
-
-// addEventListeners...
-
-document.querySelector("form").addEventListener("submit", function (event) {
-  const name = document.getElementById("name").value;
-  const surname = document.getElementById("surname").value;
-  const email = document.getElementById("email").value;
-  const age = document.getElementById("age").value;
-  const gender = document.getElementById("gender").value;
-  const message = document.getElementById("message").value;
-
-  fullNameRegex = /[0-9]/;
-
-  if (name.length < 3 || name.length > 50) {
-    alert("El nom ha de tenir com a mínim 3 caràcters i com a màxim 50.");
-  } else if (fullNameRegex.test(name)) {
-    alert("El nom no pot contenir números.");
-  }
-
-  if (surname.length < 3 || surname.length > 50) {
-    alert("El cognom ha de tenir com a mínim 3 caràcters i com a màxim 50.");
-  } else if (fullNameRegex.test(surname)) {
-    alert("El cognom no pot contenir números.");
-  }
-
-  emailRegex = /^[^@]+@[^@]+\.[^@]+$/;
-
-  if (!emailRegex.test(email)) {
-    alert("El email no és correcte.");
-  }
-
-  if (age < 18) {
-    alert("Has de ser major d'edat");
-  }
-
-  if (message.length < 10 || message.length > 200) {
-    alert("El missatge ha de tenir com a mínim 10 caràcters i com a màxim 200.");
-  }
-});
+ // Selecció dels inputs...
+ const nameInput = document.querySelector('input[name="name"]');
+ const surnameInput = document.querySelector('input[name="surname"]');
+ const emailInput = document.querySelector('input[name="email"]');
+ const ageInput = document.querySelector('input[name="age"]');
+ const genderInput = document.getElementById('gender');
+ const messageInput = document.querySelector('textarea[name="message"]');
+ 
+ // Funció per eliminar missatges d'error existents
+ function removeExistingError(input) {
+   const existingError = input.nextElementSibling;
+   if (existingError && existingError.classList.contains("error")) {
+     existingError.remove();
+   }
+ }
+ 
+ // Funció per crear i mostrar missatges d'error
+ function displayError(input, message) {
+   const error = document.createElement("div");
+   error.textContent = message;
+   error.classList.add("error");
+   input.insertAdjacentElement("afterend", error);
+ }
+ 
+ function validateName() {
+   removeExistingError(nameInput);
+   const name = nameInput.value.trim();
+   if (name.length < 3 || name.length > 50 || /\d/.test(name)) {
+     displayError(nameInput, "El nom ha de tenir entre 3 i 50 caràcters i no pot contenir números.");
+   }
+ }
+ 
+ function validateSurname() {
+   removeExistingError(surnameInput);
+   const surname = surnameInput.value.trim();
+   if (surname.length < 3 || surname.length > 50 || /\d/.test(surname)) {
+     displayError(surnameInput, "El cognom ha de tenir entre 3 i 50 caràcters i no pot contenir números.");
+   }
+ }
+ 
+ function validateEmail() {
+   removeExistingError(emailInput);
+   const email = emailInput.value.trim();
+   if (!/^[^@]+@[^@]+\.[^@]+$/.test(email)) {
+     displayError(emailInput, "Si us plau, introdueix una adreça de correu electrònic vàlida.");
+   }
+ }
+ 
+ function validateAge() {
+   removeExistingError(ageInput);
+   const age = parseInt(ageInput.value);
+   if (age < 18) {
+     alert('Has de ser major d\'edat per poder realitzar la comanda.');
+   }
+ }
+ 
+ function validateMessage() {
+   removeExistingError(messageInput);
+   const message = messageInput.value.trim();
+   if (message.length < 10 || message.length > 200) {
+     displayError(messageInput, "El missatge ha de tenir entre 10 i 200 caràcters.");
+   }
+ }
+ 
+ function validateGender() {
+   removeExistingError(genderInput);
+   if (genderInput.value === '') {
+     displayError(genderInput, 'Si us plau, selecciona el teu gènere.');
+   }
+ }
+ 
+ // addEventListeners...
+ nameInput.addEventListener('input', validateName);
+ surnameInput.addEventListener('input', validateSurname);
+ emailInput.addEventListener('input', validateEmail);
+ ageInput.addEventListener('change', validateAge);
+ genderInput.addEventListener('change', validateGender);
+ messageInput.addEventListener('input', validateMessage);
+ 
